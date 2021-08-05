40da8bd9ef4f9a99366e947af1ae364b0de10b96
Crowdbotics <team@crowdbotics.com>
2021-08-05 17:07:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "blue_sun_29522Identifier",
+  url: "https://blue_sun_29522Identifier.botics.co",
+  api: "https://blue_sun_29522Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
